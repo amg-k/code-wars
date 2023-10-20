df69164177ca91fb5615f5de05e987baa8db051b
amg-k <adamgorz@gmail.com>
2023-10-20 08:38:03 +0200

    Add logic to count number of occurence

--- MostFrequentWords.java
+++ MostFrequentWords.java
@@ -3,7 +3,7 @@ import java.util.stream.*;
 
 public class MostFrequentWords {
     public static void main(String[] args) {
-        var s = " ##//wont 'won't won't won't wont won't";
+        var s = " ##//wont 'won't won't Won't wont won't  adam ada aDam Adam ";
         topThree(s);
 
     }
@@ -12,12 +12,26 @@ public class MostFrequentWords {
         s = s.strip().replaceAll("[/+]", " ");
         s = s.strip().replaceAll(" +", " ");
         String[] sArr = s.split(" ");
-        //System.out.println(Arrays.toString(sArr));
-        var sList = Stream.of(sArr).filter(e -> e.matches("[a-zA-Z'?]*")).sorted().collect(Collectors.toList());
-        
-        sList.removeAll(new ArrayList<String>() {{ add("won't"); }} );
+        var freqMap = new HashMap<String, Integer>();
 
-        System.out.println(sList);
+        //System.out.println(Arrays.toString(sArr));
+        var sList = Stream.of(sArr)
+                          .map(e -> e.toLowerCase())
+                          .filter(e -> e.matches("[a-zA-Z'?]*"))
+                          .sorted()
+                          .collect(Collectors.toList());
+        System.out.println(sList + " " + sList.size());
+
+        while (sList.size() > 0) {
+            var word = sList.get(0);
+            var oldLen = sList.size();
+            sList.removeIf(e -> e.equals(word));
+            var num = oldLen - sList.size();
+            freqMap.put(word, num);
+        }
+        
+        System.out.println(sList + " " + sList.size());
+        System.out.println(freqMap);
 
         return null;
     }
