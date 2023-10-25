d5f5d7b48c3ef5f9f00c8e126a3bff1f4fa0def8
amg-k <adamgorz@gmail.com>
2023-10-25 20:45:48 +0200

    Add recursion solution

--- Permutation.java
+++ Permutation.java
@@ -1,23 +1,29 @@
-
+import java.util.*;
 public class Permutation {
+    static Set<String> permSet = new HashSet<>();
+    static int idx = 0;
+
     public static void main(String[] args) {
-        System.out.println(fact(13));
+        System.out.println(perm(new StringBuilder("abc"), idx));
+        List<String> l = List.copyOf(permSet);
+        System.out.println(l);
     }
 
-    static String[] perm(String phrase) {
-        var arr = new String[phrase.length()];
-        return arr;
-    }
-
-    static int fact(int n) {
-        if (n > 12) {
-            System.out.println("Input number is to high. Allowed up to '12'.");
-            return -1;
-        } else if (n == 1) return 1;
-        int f = n;
-        for (int i = n - 1; i >= 1; i--) {
-            f = f * i;
+    static Set<String> perm(StringBuilder b, int idx) {
+        if (idx == b.length()) permSet.add(b.toString());
+        else {
+            for (int i = idx; i <b.length(); i++) {
+                swap(i, idx, b);
+                perm(b, idx + 1);
+                swap(i, idx, b);
+            }
         }
-        return f;
+        return permSet;
     }
+
+    static void swap(int idx1, int idx2, StringBuilder b) {
+        char temp = b.charAt(idx1);
+        b.setCharAt(idx1, b.charAt(idx2));
+        b.setCharAt(idx2, temp);
+    } 
 }
